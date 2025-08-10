8e3bf155cb05663c8cfa2dbf4b35ada5f389e037
Amit Kiran Kar <141171486+AmitKK10@users.noreply.github.com>
2025-08-10 13:36:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000', // local dev
-    'https://pizza-app-frontend-ebon.vercel.app' // deployed frontend
+    'https://pizza-app-frontend-jzcn.vercel.app/' // deployed frontend
   ],
   credentials: true
 }));
